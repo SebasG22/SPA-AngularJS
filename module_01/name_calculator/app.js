5644e8b3b3fad710a214f678f5769eef1fe76302
Sebastián Guevara <rlxsebas@gmail.com>
2016-11-14 15:05:26 +0000

    Module 01: Name Calculator - Calculate the numeric value of person name

--- module_01/name_calculator/app.js
+++ module_01/name_calculator/app.js
@@ -4,11 +4,23 @@
   angular.module('nameCaculator',[])
 
   .controller('nameCaculatorController',function ($scope) {
-$scope.name="Sebastian";
-$scope.sayHello= function () {
-  return "Hello Sebas !";
-}
+$scope.name="";
+$scope.totalValue=0;
 
+$scope.displayNumeric= function (){
+  
+  var totalNameValue = calculateNumericForString($scope.name); //get the total value
+  $scope.totalValue=totalNameValue;
+};
+
+
+function calculateNumericForString(string){
+  var totalStringValue =0 ;
+  for (var i=0 ; i < string.length;i++){
+    totalStringValue+=string.charCodeAt(i);
+  }
+  return totalStringValue;
+}
   });
 
 })();
