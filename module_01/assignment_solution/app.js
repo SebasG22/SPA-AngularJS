2e69d5da5b93ebfbc1fca2e0170cd124afa4107a
Sebastián Guevara <rlxsebas@gmail.com>
2016-11-16 02:02:52 +0000

    Module 01: Assigment Solution - Adding ng-style for tthe textbox

--- module_01/assignment_solution/app.js
+++ module_01/assignment_solution/app.js
@@ -13,45 +13,40 @@
         
         $scope.message = "Message Report: Blank";
         
-        $scope.style={color:'black'};
-        console.log(($scope.lunchMenu));
+        $scope.styleMessage={color:'black'};
+        
+        $scope.styleBox={};
 
+        console.log(($scope.lunchMenu));
 
         $scope.checkLunchMenu= function (){
             
             console.log($scope.lunchMenu);
+            
             if($scope.lunchMenu != undefined && $scope.lunchMenu !=""){
             
-            //console.log("Lunch:"+$scope.lunchMenu.length);
+                //console.log("Lunch:"+$scope.lunchMenu.length);
                 
                 var itemsLunch = $scope.lunchMenu.split(',');
           
                 console.log(itemsLunch);  
                 
-                                if(itemsLunch.length<=3){
+                if(itemsLunch.length<=3){
                     $scope.message="Enjoy!";
-                            $scope.style={color:'green'};
-
-
+                    $scope.styleMessage={color:'green'};
+                    $scope.styleBox={border:'1px solid green'};
                 }
                 else{
-                    
                     $scope.message="Too much!";
-                                                $scope.style={color:'green'};
-
-
+                    $scope.styleMessage={color:'green'};
+                    $scope.styleBox={border:'1px solid green'};
                 }
-            
             } 
             else{
                 $scope.message = "Message Report: Please enter data first";
-                                            $scope.style={color:'red'};
-
+                $scope.styleMessage={color:'red'};
+                $scope.styleBox={border:'1px solid red'};
             }
-            
-
-    
-            
         }
     }
     
