8ad0afe8e33fcd06b3e58946bc1efdd49e9c3b91
Sebastián Guevara <rlxsebas@gmail.com>
2016-12-03 16:39:08 +0000

    Creating a ItemList and Display it in the view using ng-repeat

--- module_02/assigment_solution/script.js
+++ module_02/assigment_solution/script.js
@@ -3,7 +3,7 @@
  'use strict';
  angular.module("ShoppingListCheckOff",[])
  .controller("ToBuyController",ToBuyController)
- .controller("AlreadyBoughtController")
+ .controller("AlreadyBoughtController",AlreadyBoughtController)
  .service("ShoppingListCheckOffService",ShoppingListCheckOffService);
  
  ToBuyController.$inject=['ShoppingListCheckOffService'];
@@ -11,11 +11,36 @@
  function ToBuyController(ShoppingListCheckOffService) {
      var itemAdder = this;
      
-     itemAdder.itemName = "";
-     itemAdder.itemQuantity = "";
+     itemAdder.itemsPre = [
+         {
+         name:"Cookies",
+         quantity:5
+         },
+         {
+         name:"Tomatoes",
+         quantity:2
+         },
+         {
+         name:"Lettuce",
+         quantity:3
+         },
+         {
+         name:"Butter",
+         quantity:1
+         },
+         {
+         name:"Suggar",
+         quantity:4
+         },
+         {
+         name:"Carrots",
+         quantity:2
+         }
+         
+     ];
      
-     itemAdder.addItem = function(){
-         ShoppingListCheckOffService.addItem(itemAdder.itemName,itemAdder.itemQuantity);
+     itemAdder.addItem = function(item){
+         ShoppingListCheckOffService.addItem(item);
      }
  }
  
@@ -23,8 +48,29 @@
  
 function AlreadyBoughtController (ShoppingListCheckOffService){
     
+    var listBought= this;
+    
+    listBought.items= ShoppingListCheckOffService.getItems();
 }
- 
+
+function ShoppingListCheckOffService(){
     
+    var service = this;
     
+    var items=[];
+    
+    service.addItem = function (itemName, itemQuantity) {
+        var item = {
+            name: itemName,
+            quantity: itemQuantity
+        }
+        
+        items.push(item);
+
+    }
+    
+    service.getItems = function(){
+    return items;       
+    }
+}
 })();
